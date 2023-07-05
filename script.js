c76abb3148de69e5e53ae24ff50a2c9aa8f76310
Ganiyu Sodiq <gsolaniyi@gmail.com>
2023-07-05 09:18:40 +0100

    figure out how to get the input tinto your code

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function createTodo(){
+    const todos = {
+        container : document.createElement('div'),
+        todoInput : document.createElement('input'),
+        removeButton : document.createElement('button')
+    }
+
+
+    // 
+
+    // let onpageInput = document.getElementById('new-todo-input').value
+    // console.log(onpageInput) 
+
+    todos.removeButton.innerText = 'Remove Todo'
+
+    todos.container.appendChild(todos.todoInput)
+    todos.container.appendChild(todos.removeButton)
+
+    return todos.container
+}
+
+function main(){
+    const todosBox = document.getElementById('todos')
+    todosBox.appendChild(createTodo())
+}
+
+const todoButton = document.getElementById('add-todo-btn')
+todoButton.addEventListener('click', () => {
+    let onpageInput = document.getElementById('new-todo-input').value
+    console.log(onpageInput) 
+    main()
+})
+
+
